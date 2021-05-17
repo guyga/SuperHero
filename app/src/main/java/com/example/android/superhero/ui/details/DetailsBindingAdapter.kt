27b05e2c014283e69e56eb67addca9aabea039bf
Guy Gur Arie <guy.ga@action-item.co.il>
2021-05-17 09:02:10 +0300

    details screen ui

--- app/src/main/java/com/example/android/superhero/ui/details/DetailsBindingAdapter.kt
+++ app/src/main/java/com/example/android/superhero/ui/details/DetailsBindingAdapter.kt
@@ -27,3 +27,33 @@ fun bindHerosPublisher(textView: TextView, publisher: String?) {
         value = textView.resources.getString(R.string.unknown)
     textView.text = textView.resources.getString(R.string.details_publisher, value)
 }
+
+@BindingAdapter(value = ["herosGAName", "herosGroupAffiliations"])
+fun bindHerosGroupAffiliations(textView: TextView, name: String, groupAff: String?) {
+    var value = groupAff
+    if (value == null || value == "-")
+        value = textView.resources.getString(R.string.unknown)
+
+    textView.text = textView.resources.getString(R.string.details_group_affiliation, name, value)
+}
+
+@BindingAdapter("herosGender")
+fun bindHerosGender(textView: TextView, gender: String?) {
+    var value = gender
+    if (value == null || value == "-")
+        value = textView.resources.getString(R.string.unknown)
+
+    textView.text = textView.resources.getString(R.string.details_gender, value)
+}
+
+@BindingAdapter(value = ["herosPower", "herosSpeed"])
+fun bindHerosStats(textView: TextView, power: String?, speed: String?) {
+    var powerVal = power
+    if (powerVal == null || powerVal == "null")
+        powerVal = textView.resources.getString(R.string.unknown)
+    var speedVal = speed
+    if (speedVal == null || speedVal == "null")
+        speedVal = textView.resources.getString(R.string.unknown)
+
+    textView.text = textView.resources.getString(R.string.details_stats, powerVal, speedVal)
+}
