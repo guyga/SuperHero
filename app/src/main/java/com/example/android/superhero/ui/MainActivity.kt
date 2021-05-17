f86656a7ef2607d1a855a36612f38718ad544f07
Guy Gur Arie <guy.ga@action-item.co.il>
2021-05-17 08:26:16 +0300

    app bar fix

--- app/src/main/java/com/example/android/superhero/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/superhero/ui/MainActivity.kt
@@ -6,24 +6,31 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.android.superhero.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        setupActionBar(navController)
+        navController = navHostFragment.navController
+        setupActionBar()
     }
 
-    private fun setupActionBar(navController: NavController) {
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+    private fun setupActionBar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.setupWithNavController(navController, appBarConfiguration)
         setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
